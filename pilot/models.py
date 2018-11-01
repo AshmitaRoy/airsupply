bba4073bfc47325b80398dd2718a08f3e7b7122a
Junhao Dong <junhao.dong96@gmail.com>
2018-11-01 22:00:53 +0800

    Add constraints for DecimalFields

--- pilot/models.py
+++ pilot/models.py
@@ -1,9 +1,12 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(models.User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     is_clinic_manager = models.BooleanField(default=False)
     is_dispatcher = models.BooleanField(default=False)
 
@@ -15,18 +18,18 @@ class Dispatcher(UserProfile):
 
 class Clinic(models.Model):
     clinic_manager = models.ForeignKey(ClinicManager, on_delete=models.CASCADE)
-    name = models.CharField(length=200)
-    latitude = models.DecimalField()
-    longitude = models.DecimalField()
+    name = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=10, decimal_places=6)
     altitude = models.IntegerField()
     pass
 
 class Item(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(length=200)
+    name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
-    price = models.DecimalField(decimal_places=2)
-    shipping_weight = models.DecimalField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    shipping_weight = models.DecimalField(max_digits=4, decimal_places=2)
     image = models.ImageField()
 
 class Order(models.Model):
