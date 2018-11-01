f558346790e6486f9baf0e92b0812954b150677a
Junhao Dong <junhao.dong96@gmail.com>
2018-11-01 21:52:42 +0800

    Fix syntax

--- airsupply/settings.py
+++ airsupply/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pilot.apps.PilotConfig'
+    'pilot.apps.PilotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
